c0fb6a708608f247ffb5aea54b52812bd4daf111
Teylorrolyet <130723772+Teylorrolyet@users.noreply.github.com>
2026-02-09 19:33:53 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME='portal-bosque-v1';
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c=>{
+      return c.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
